3fe12eb38663dba77f337a60875e4f16acf6c5f0
cong <zc@cz9.cn>
2020-04-26 15:09:39 +0800

    保种 TOP

--- keeptop.php
+++ keeptop.php
@@ -22,10 +22,10 @@ begin_main_frame('',true);
     print("<td class=\"colhead\">核算官种数量</td>");
 	print("</tr>");
 	$res = sql_query(" SELECT t1.userid,sum(t2.size) as tsize,count(t1.id) as sl,
-    sum(case when t2.team in (1,2,3,4,12,17,20,21,22) then t2.size else 0 end)  as osize,
-    sum(case when t2.team in (1,2,3,4,12,17,20,21,22) then 1 else 0 end)  as osl,
-    sum(case when t2.team in (1,2,3,4,12,17,20,21,22) then t2.size else t2.size/4 end)  as csize,
-    sum(case when t2.team in (1,2,3,4,12,17,20,21,22) then 1 else 0.25 end)  as csl
+    sum(case when t2.team in (6) then t2.size else 0 end)  as osize,
+    sum(case when t2.team in (6) then 1 else 0 end)  as osl,
+    sum(case when t2.team in (6) then t2.size else t2.size/4 end)  as csize,
+    sum(case when t2.team in (6) then 1 else 0.25 end)  as csl
     FROM `peers` as t1 left join torrents as t2 on t1.torrent=t2.id where 1 group by t1.userid having csize order by csize desc limit ".($_REQUEST['limit']?intval($_REQUEST['limit']):100));
 	$i=0;
 	while($row = mysql_fetch_array($res))
